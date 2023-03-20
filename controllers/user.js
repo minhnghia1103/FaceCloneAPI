2cf75420500c7ca727046b4afe51e2fc64f262ab
Minh Nghia <dankkingmeo2@gmail.com>
2023-03-20 22:26:58 +0700

    'Api-user'

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const UsersModel = require("../models/usersModel");
+var jwt = require("jsonwebtoken");
 
 module.exports.getAccount = async (req, res) => {
   UsersModel.find()
@@ -11,21 +12,48 @@ module.exports.getAccount = async (req, res) => {
 };
 
 module.exports.addAccount = async (req, res) => {
-  const { fullname, email, password, sex } = req.body;
-  await UsersModel.create({
-    fullname: fullname,
-    email: email,
-    password: password,
-    sex: sex,
-  })
-    .then((data) => {
-      return res.status(200).send({
-        status: "success",
-        message: "Account added, register successfully !",
-        data: data,
-      });
+  const { fullname, email, password } = req.body;
+  const checkExist = await UsersModel.findOne({ email: email });
+  if (!checkExist) {
+    await UsersModel.create({
+      fullname: fullname,
+      email: email,
+      password: password,
     })
-    .catch((err) => {
-      return res.status(500).json(err.message);
+      .then((data) => {
+        return res.status(200).send({
+          status: "success",
+          message: "Account added, register successfully !",
+          data: data,
+        });
+      })
+      .catch((err) => {
+        return res.status(500).json(err.message);
+      });
+  } else {
+    return res.send({
+      status: "existEmail",
     });
+  }
+};
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const checkExist = await UsersModel.findOne({ email: email, password: password });
+  if (checkExist) {
+    try {
+      const _id = checkExist._id;
+      return res.status(200).send({
+        status: true,
+        token: jwt.sign({ _id }, "ngulol"),
+        data: checkExist,
+      });
+    } catch (e) {
+      return res.status(500).json(e.message);
+    }
+  } else {
+    return res.send({
+      status: false,
+    });
+  }
 };
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = 8080;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 
-const { getAccount, addAccount } = require("./controllers/user");
+const { getAccount, addAccount, login } = require("./controllers/user");
 
 app.get("/allusers", getAccount);
 app.post("/adduser", addAccount);
+app.post("/login", login);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
